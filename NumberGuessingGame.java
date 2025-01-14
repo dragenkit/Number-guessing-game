53a5f6e3b06e0ef9fb8082a2d262f155a216f42a
dragenkit <marvinauner47@gmail.com>
2025-01-14 23:17:47 +0100

    add getGuessedNumber()

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String[] args) {
+        getGuessedNumber();
+    }
 
+
+    public static int getGuessedNumber() {
+        Scanner userGuess = new Scanner(System.in);
+        System.out.println("Guess the Number:");
+        int guessedNumber = userGuess.nextInt();
+        return guessedNumber;
     }
 }
