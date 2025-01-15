83b8740d3a3e92e5f0f1c559575e8c88cecfa034
dragenkit <marvinauner47@gmail.com>
2025-01-15 20:15:43 +0100

    added method generateTargetNumber

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        getGuessedNumber();
+        generateTargetNumber();
+        promptUserGuess();
     }
 
-
-    public static int getGuessedNumber() {
+    // lets the player choose his guessed number
+    public static int promptUserGuess() {
         Scanner userGuess = new Scanner(System.in);
         System.out.println("Guess the Number:");
         int guessedNumber = userGuess.nextInt();
         return guessedNumber;
+    }
+        // Returns a random number from 1 to 100
+    public static int generateTargetNumber() {
+            int targetNumber = (int)(Math.random() * 101);
+            System.out.println(targetNumber);
+            return targetNumber;
     }
 }
